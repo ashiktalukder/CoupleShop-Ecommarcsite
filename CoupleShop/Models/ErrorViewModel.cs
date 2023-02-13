7d1201a6bc012f0921789a8404a98accf8c7327a
Ashik-ND <Ashik-ND@DESKTOP-Q2EQLQQ>
2023-02-13 22:53:19 +0600

    Add project files.

---
+++ CoupleShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoupleShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
